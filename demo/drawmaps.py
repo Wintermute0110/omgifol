59cb3b3316fb873f5f9c859210afd4892659b87c
Wintermute0110 <wintermute0110@gmail.com>
2017-03-11 21:46:46 +0900

    Include scale parameter and other improvements.

--- demo/drawmaps.py
+++ demo/drawmaps.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
+# Original from Fredrik Johansson, 2006-12-11 
+# Incorporates code by Frans P. de Vries, 2016-04-26 
+# Incorporates code by Wintermute0110, 2017-03-11
+#
+#  720p -- 1280x720
+# 1080p -- 1920x1080
 #
 import sys
 import os
+import getopt
+import pprint
 from PIL import Image, ImageDraw
 
 # --- Add OMG module to Python path ---
@@ -17,80 +25,120 @@ sys.path.insert(0, moduledir)
 from omg import *
 
 # -------------------------------------------------------------------------------------------------
+# Globals
+# -------------------------------------------------------------------------------------------------
+global_verbose = True
+border = 25
+scales = 0
+total = 0
+
+# -------------------------------------------------------------------------------------------------
 # Functions
 # -------------------------------------------------------------------------------------------------
 def drawmap(wad, name, filename, width, format):
-    xsize = width - 8
-
+    global scales, total
+    maxpixels = width
+    reqscale = 0
     edit = MapEditor(wad.maps[name])
-    xmin = ymin = 32767
-    xmax = ymax = -32768
+    
+   # determine scale = map area unit / pixel
+    xmin = min([v.x for v in edit.vertexes])
+    xmax = max([v.x for v in edit.vertexes])
+    ymin = min([-v.y for v in edit.vertexes])
+    ymax = max([-v.y for v in edit.vertexes])
+    xsize = xmax - xmin
+    ysize = ymax - ymin
+    scale = (maxpixels-border*2) / float(max(xsize, ysize))
+
+    # tally for average scale or compare against requested scale
+    if reqscale == 0:
+        scales = scales + scale
+        total = total + 1
+    else:
+        if scale > 1.0 / reqscale:
+            scale = 1.0 / reqscale
+
+    # convert all numbers to image space
+    xmax = int(xmax*scale); xmin = int(xmin*scale)
+    ymax = int(ymax*scale); ymin = int(ymin*scale)
+    xsize = int(xsize*scale) + border*2;
+    ysize = int(ysize*scale) + border*2;
     for v in edit.vertexes:
-        xmin = min(xmin, v.x)
-        xmax = max(xmax, v.x)
-        ymin = min(ymin, -v.y)
-        ymax = max(ymax, -v.y)
+        v.x = v.x * scale; v.y = -v.y * scale 
 
-    scale = xsize / float(xmax - xmin)
-    xmax = int(xmax * scale)
-    xmin = int(xmin * scale)
-    ymax = int(ymax * scale)
-    ymin = int(ymin * scale)
-
-    for v in edit.vertexes:
-        v.x = v.x * scale
-        v.y = -v.y * scale
-
-    im = Image.new('RGB', ((xmax - xmin) + 8, (ymax - ymin) + 8), (255,255,255))
+    # --- Create image ---
+    if global_verbose:
+        print('drawmap() xsize = {0}'.format(xsize))
+        print('drawmap() ysize = {0}'.format(ysize))
+    im = Image.new('RGB', (xsize, ysize), (255,255,255))
     draw = ImageDraw.Draw(im)
 
     edit.linedefs.sort(lambda a, b: cmp(not a.two_sided, not b.two_sided))
 
     for line in edit.linedefs:
-         p1x = edit.vertexes[line.vx_a].x - xmin + 4
-         p1y = edit.vertexes[line.vx_a].y - ymin + 4
-         p2x = edit.vertexes[line.vx_b].x - xmin + 4
-         p2y = edit.vertexes[line.vx_b].y - ymin + 4
-
+         p1x = edit.vertexes[line.vx_a].x - xmin + border
+         p1y = edit.vertexes[line.vx_a].y - ymin + border
+         p2x = edit.vertexes[line.vx_b].x - xmin + border
+         p2y = edit.vertexes[line.vx_b].y - ymin + border
          color = (0, 0, 0)
-         if line.two_sided:
-             color = (144, 144, 144)
-         if line.action:
-             color = (220, 130, 50)
+         if line.two_sided: color = (144, 144, 144)
+         if line.action:    color = (220, 130, 50)
 
+         # draw several lines to simulate thickness 
          draw.line((p1x, p1y, p2x, p2y), fill=color)
-         # draw.line((p1x+1, p1y, p2x+1, p2y), fill=color)
-         # draw.line((p1x-1, p1y, p2x-1, p2y), fill=color)
-         # draw.line((p1x, p1y+1, p2x, p2y+1), fill=color)
-         # draw.line((p1x, p1y-1, p2x, p2y-1), fill=color)
+         draw.line((p1x+1, p1y, p2x+1, p2y), fill=color)
+         draw.line((p1x-1, p1y, p2x-1, p2y), fill=color)
+         draw.line((p1x, p1y+1, p2x, p2y+1), fill=color)
+         draw.line((p1x, p1y-1, p2x, p2y-1), fill=color)
 
     # --- Draw scale (or grid or XY axis) ---
-    scale_size = 654 * scale
-    draw.line((10, 10, 10 + scale_size, 10), fill=(255, 0, 0))
+    # scale_size = 654 * scale
+    # draw.line((10, 10, 10 + scale_size, 10), fill=(255, 255, 0))
 
+    # --- Draw XY axis ---
+    # scale_size = 654 * scale
+    # draw.line((10, 10, 10 + scale_size, 10), fill=(255, 0, 0))
+
+    # --- Save image file ---
     del draw
-
     im.save(filename, format)
 
 # -------------------------------------------------------------------------------------------------
 # main ()
 # -------------------------------------------------------------------------------------------------
-if len(sys.argv) < 5:
+if len(sys.argv) < 2:
     print('Omgifol script: draw maps to image files')
-    print('Draw all maps whose names match the given pattern (eg E?M4 or MAP*) '
-          'to image files of a given format (PNG, BMP, etc). width specifies the '
-          'desired width of the output images.\n')
-    print('Usage: drawmaps.py source.wad [pattern [width [format]]]')
-    print('pattern examples: "E?M?", "MAP01", "MAP*" or all')
-    print('width is in pixels')
-    print('format may be PNG, BMP, JPEG. Defaults to JPEG')
-else:
-    print('Loading WAD "{0}" ...'.format(sys.argv[1]))
-    inwad = WAD()
-    inwad.from_file(sys.argv[1])
-    width = int(sys.argv[3])
-    format = sys.argv[4].upper()
-    for name in inwad.maps.find(sys.argv[2]):
-        filename = os.path.splitext(sys.argv[1])[0] + '_' + name + '.' + format.lower()
-        print('Drawing map {0} on file "{1}"'.format(name, filename))
-        drawmap(inwad, name, filename, width, format)
+    print('Draw all maps whose names match the given pattern (eg E?M4 or MAP*)')
+    print('to image files of a given format (PNG, BMP, etc). width specifies the')
+    print('desired width of the output images.\n')
+    print('Usage: drawmaps.py [-p pattern] [-w width [-f format] source.wad')
+    print('  -p pattern  Patterns may be "E?M?", "MAP01", "MAP*"')
+    print('              Defaults to all level (pattern "*")')
+    print('  -w width    Width in pixels. Defaults to 1920 (for a 1920x1080 image)')
+    print('  -f format   May be PNG, BMP, JPEG. Defaults to PNG')
+    sys.exit(1)
+
+# --- Parse arguments ---
+opts, args = getopt.getopt(sys.argv[1:], 'p:w:f:')
+pprint.pprint(opts)
+pprint.pprint(args)
+wad_filename = args[0]
+pattern = '*'
+width = 1920
+format = 'PNG'
+for o, a in opts:
+    if   o == '-p': pattern = a
+    elif o == '-w': width = int(a)
+    elif o == '-f': format = a
+    else:
+        assert False, "Unhandled option"
+
+# --- Load WAD ---
+print('Loading WAD "{0}" ...'.format(wad_filename))
+inwad = WAD()
+inwad.from_file(wad_filename)
+for name in inwad.maps.find(pattern):
+    filename = os.path.splitext(wad_filename)[0] + '_' + name + '.' + format.lower()
+    print('Drawing map {0} on file "{1}"'.format(name, filename))
+    drawmap(inwad, name, filename, width, format)
+sys.exit(0)
