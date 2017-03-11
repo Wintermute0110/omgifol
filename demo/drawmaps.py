15b79d7b0f76f535743c77e4bbf04dcdd0ef9999
Wintermute0110 <wintermute0110@gmail.com>
2017-03-11 18:41:58 +0900

    Allows loading parent module.
    Modernise code a bit to run on recent Python version.

--- demo/drawmaps.py
+++ demo/drawmaps.py
@@ -1,7 +1,24 @@
-from omg import *
+#!/usr/bin/python
+#
 import sys
-import Image, ImageDraw
+import os
+from PIL import Image, ImageDraw
 
+# --- Add OMG module to Python path ---
+real_path  = os.path.realpath(__file__)
+currentdir = os.path.dirname(real_path)
+parentdir  = os.path.dirname(currentdir)
+moduledir  = os.path.dirname(parentdir)
+# print('real_path  "{0}"'.format(real_path))
+# print('currentdir "{0}"'.format(currentdir))
+# print('parentdir  "{0}"'.format(parentdir))
+# print('moduledir  "{0}"'.format(moduledir))
+sys.path.insert(0, moduledir) 
+from omg import *
+
+# -------------------------------------------------------------------------------------------------
+# Functions
+# -------------------------------------------------------------------------------------------------
 def drawmap(wad, name, filename, width, format):
     xsize = width - 8
 
@@ -42,33 +59,38 @@ def drawmap(wad, name, filename, width, format):
              color = (220, 130, 50)
 
          draw.line((p1x, p1y, p2x, p2y), fill=color)
-         draw.line((p1x+1, p1y, p2x+1, p2y), fill=color)
-         draw.line((p1x-1, p1y, p2x-1, p2y), fill=color)
-         draw.line((p1x, p1y+1, p2x, p2y+1), fill=color)
-         draw.line((p1x, p1y-1, p2x, p2y-1), fill=color)
+         # draw.line((p1x+1, p1y, p2x+1, p2y), fill=color)
+         # draw.line((p1x-1, p1y, p2x-1, p2y), fill=color)
+         # draw.line((p1x, p1y+1, p2x, p2y+1), fill=color)
+         # draw.line((p1x, p1y-1, p2x, p2y-1), fill=color)
+
+    # --- Draw scale (or grid or XY axis) ---
+    scale_size = 654 * scale
+    draw.line((10, 10, 10 + scale_size, 10), fill=(255, 0, 0))
 
     del draw
 
     im.save(filename, format)
 
-
-#import psyco
-#psyco.full()
-
-if (len(sys.argv) < 5):
-    print "\n    Omgifol script: draw maps to image files\n"
-    print "    Usage:"
-    print "    drawmaps.py source.wad pattern width format\n"
-    print "    Draw all maps whose names match the given pattern (eg E?M4 or MAP*)"
-    print "    to image files of a given format (PNG, BMP, etc). width specifies the"
-    print "    desired width of the output images."
+# -------------------------------------------------------------------------------------------------
+# main ()
+# -------------------------------------------------------------------------------------------------
+if len(sys.argv) < 5:
+    print('Omgifol script: draw maps to image files')
+    print('Draw all maps whose names match the given pattern (eg E?M4 or MAP*) '
+          'to image files of a given format (PNG, BMP, etc). width specifies the '
+          'desired width of the output images.\n')
+    print('Usage: drawmaps.py source.wad [pattern [width [format]]]')
+    print('pattern examples: "E?M?", "MAP01", "MAP*" or all')
+    print('width is in pixels')
+    print('format may be PNG, BMP, JPEG. Defaults to JPEG')
 else:
-
-    print "Loading %s..." % sys.argv[1]
+    print('Loading WAD "{0}" ...'.format(sys.argv[1]))
     inwad = WAD()
     inwad.from_file(sys.argv[1])
     width = int(sys.argv[3])
     format = sys.argv[4].upper()
     for name in inwad.maps.find(sys.argv[2]):
-        print "Drawing %s" % name
-        drawmap(inwad, name, name + "_map" + "." + format.lower(), width, format)
+        filename = os.path.splitext(sys.argv[1])[0] + '_' + name + '.' + format.lower()
+        print('Drawing map {0} on file "{1}"'.format(name, filename))
+        drawmap(inwad, name, filename, width, format)
