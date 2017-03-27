dd8330b19f140a8b6d605d6d09b8f4cc5311fa72
Wintermute0110 <wintermute0110@gmail.com>
2017-03-27 20:54:46 +0900

    Print number of level items on a table.

--- demo/listmaps.py
+++ demo/listmaps.py
@@ -30,16 +30,15 @@ else:
     # pprint.pprint(inwad.groups)
 
     print('Number of levels {0}'.format(inwad.maps._n))
+    print('Num | Name  | Things | Vertexes | Linedefs | Sidedefs | Sectors |   Segs | SSectors | Nodes |')
+    print('----|-------|--------|----------|----------|----------|---------|--------|----------|-------|')
     for i, name in enumerate(inwad.maps):
         edit = MapEditor(inwad.maps[name])
-        # print('>>>>> map object')
+        # print('>>>>> inwad.maps[name] object')
         # pprint.pprint(vars(inwad.maps[name]))
         # print('>>>>> edit object attribute keys')
         # pprint.pprint(edit.__dict__.keys())
-        print('Map number {0:2d} name {1}'.format(i, name))
-        print('  Things   {0:5d}'.format(len(edit.things)))
-        print('  Vertexes {0:5d}'.format(len(edit.vertexes)))
-        print('  Linedefs {0:5d}'.format(len(edit.linedefs)))
-        print('  Sidedefs {0:5d}'.format(len(edit.sidedefs)))
-        print('  Sectors  {0:5d}'.format(len(edit.sectors)))
-        # print('  Nodes    {0:5d}'.format(len(edit.nodes)))
+        fmt_str = ' {0:2d} | {1:>5} | {2:6d} | {3:8d} | {4:8d} | {5:8d} | {6:7d} | {7:6d} | {8:8d} | {9:5d} |'
+        print(fmt_str.format(i, name, 
+                             len(edit.things),  len(edit.vertexes), len(edit.linedefs), len(edit.sidedefs), 
+                             len(edit.sectors), len(edit.segs),     len(edit.ssectors), len(edit.nodes)))
