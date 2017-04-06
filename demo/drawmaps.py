00e1cdff0fe02467ff1899aba1af0eb9d59e65d2
Wintermute0110 <wintermute0110@gmail.com>
2017-04-06 16:00:28 +0900

    New object to encapsulate linear transformations.

--- demo/drawmaps.py
+++ demo/drawmaps.py
@@ -36,7 +36,7 @@ from omg import *
 # Globals
 # -------------------------------------------------------------------------------------------------
 VERBOSE = True
-BORDER_PIXELS = 0
+BORDER_PERCENT = 5
 
 MAP_LINEDEFS = 100
 MAP_SECTORS  = 200
@@ -46,14 +46,67 @@ MAP_VERTEXES = 400
 # -------------------------------------------------------------------------------------------------
 # Vars
 # -------------------------------------------------------------------------------------------------
-class BBox:
-    def __init__(self, left, right, bottom, top):
+class LinearTransform:
+    def __init__(self, left, right, bottom, top, px_size, py_size, border):
         self.left   = left
         self.right  = right
         self.bottom = bottom
         self.top    = top
-        self.xsize  = right - left
-        self.ysize  = top - bottom
+        self.x_size  = right - left
+        self.y_size  = top - bottom
+        # --- Shift map in x or y direction ---
+        self.pan_x  = 0
+        self.pan_y  = 0
+
+        print('LinearTransform() left       = {0}'.format(left))
+        print('LinearTransform() right      = {0}'.format(right))
+        print('LinearTransform() bottom     = {0}'.format(bottom))
+        print('LinearTransform() top        = {0}'.format(top))
+        print('LinearTransform() x_size     = {0}'.format(self.x_size))
+        print('LinearTransform() y_size     = {0}'.format(self.y_size))
+
+        # --- Calculate scale in [pixels] / [map_unit] ---
+        self.px_size = px_size
+        self.py_size = py_size
+        self.border  = border
+        self.border_x = px_size * border / 100
+        self.border_y = py_size * border / 100
+        self.pxsize_nob = px_size - 2*self.border_x
+        self.pysize_nob = py_size - 2*self.border_y
+        self.x_scale = self.pxsize_nob / float(self.x_size)
+        self.y_scale = self.pysize_nob / float(self.y_size)
+        if self.x_scale < self.y_scale:
+            self.scale   = self.x_scale
+            self.xoffset = self.border_x
+            self.yoffset = (py_size - int(self.y_size*self.scale)) / 2
+        else:
+            self.scale   = self.y_scale
+            self.xoffset = (px_size - int(self.x_size*self.scale)) / 2
+            self.yoffset = self.border_y
+        print('LinearTransform() px_size    = {0}'.format(px_size))
+        print('LinearTransform() py_size    = {0}'.format(py_size))
+        print('LinearTransform() border     = {0}'.format(border))
+        print('LinearTransform() border_x   = {0}'.format(self.border_x))
+        print('LinearTransform() border_y   = {0}'.format(self.border_y))
+        print('LinearTransform() pxsize_nob = {0}'.format(self.pxsize_nob))
+        print('LinearTransform() pysize_nob = {0}'.format(self.pysize_nob))
+        print('LinearTransform() xscale     = {0}'.format(self.x_scale))
+        print('LinearTransform() yscale     = {0}'.format(self.y_scale))
+        print('LinearTransform() scale      = {0}'.format(self.scale))
+        print('LinearTransform() xoffset    = {0}'.format(self.xoffset))
+        print('LinearTransform() yoffset    = {0}'.format(self.yoffset))
+
+    def MapToScreen(self, map_x, map_y):
+        screen_x = self.scale * (+map_x - self.left) + self.xoffset
+        screen_y = self.scale * (-map_y + self.top)  + self.yoffset
+
+        return (screen_x, screen_y)
+
+    def ScreenToMap(self, screen_x, screen_y):
+        map_x = +(screen_x - self.xoffset + self.scale * self.left) / self.scale
+        map_y = -(screen_y - self.yoffset - self.scale * self.top) / self.scale
+
+        return (map_x, map_y)
 
 # See https://github.com/chocolate-doom/chocolate-doom/blob/sdl2-branch/src/doom/am_map.c
 class ColorScheme:
@@ -99,73 +152,72 @@ def draw_thick_line(draw, p1x, p1y, p2x, p2y, color):
     draw.line((p1x, p1y+1, p2x, p2y+1), fill = color)
     draw.line((p1x, p1y-1, p2x, p2y-1), fill = color)
 
+def draw_axis(draw, LT, color):
+    (pxzero, pyzero) = LT.MapToScreen(0, 0)
+    draw.line((0, pyzero, LT.px_size, pyzero), fill = color)
+    draw.line((pxzero, 0, pxzero, LT.py_size), fill = color)
+
+#
+# A level must be contained within a 16384-unit radius as measured from its center point.
+# Point A is the top-left corner.
+#
+# A---------B---------C   A-C gap 327 map units
+# |         |         |   A-B gap 163 map units
+# |         E         |   A-D gap is 163/2 map units
+# D                   F   B-E gap is 163/4 map units
+#
+def draw_scale(draw, LT, color):
+    (A_px, A_py) = LT.MapToScreen(LT.left, LT.top)
+    (B_px, B_py) = LT.MapToScreen(LT.left+163, LT.top)
+    (C_px, C_py) = LT.MapToScreen(LT.left+327, LT.top)
+    (D_px, D_py) = LT.MapToScreen(LT.left, LT.top-163/2)
+    (E_px, E_py) = LT.MapToScreen(LT.left+163, LT.top-163/4)
+    (F_px, F_py) = LT.MapToScreen(LT.left+327, LT.top-163/2)
+
+    draw.line((A_px, A_py, C_px, C_py), fill = color) # A -> C
+    draw.line((A_px, A_py, D_px, D_py), fill = color) # A -> D
+    draw.line((B_px, B_py, E_px, E_py), fill = color) # B -> E
+    draw.line((C_px, C_py, F_px, F_py), fill = color) # C -> F
+
 #
 # In the future draw a triangle like Vanilla Doom
-#
+# https://github.com/chocolate-doom/chocolate-doom/blob/sdl2-branch/src/doom/am_map.c#L186
+# https://github.com/chocolate-doom/chocolate-doom/blob/sdl2-branch/src/doom/am_map.c#L1314
 RADIUS = 4
-def draw_thing(draw, p1x, p1y, angle, color):
+def draw_thing(draw, map_x, map_y, angle, color):
     draw.ellipse((p1x-RADIUS, p1y-RADIUS, p1x+RADIUS, p1y+RADIUS), outline = color)
 
 # -------------------------------------------------------------------------------------------------
 # Top level map drawing functions
 # -------------------------------------------------------------------------------------------------
-def drawmap_fit(wad, map_name, filename, format, map_type, pxsize, pysize, cscheme):
-    pxsize_noborder = pxsize - BORDER_PIXELS*2
-    pysize_noborder = pysize - BORDER_PIXELS*2
-    if VERBOSE:
-        print('drawmap_fit() pxsize          = {0}'.format(pxsize))
-        print('drawmap_fit() pysize          = {0}'.format(pysize))
-        print('drawmap_fit() pxsize_noborder = {0}'.format(pxsize_noborder))
-        print('drawmap_fit() pysize_noborder = {0}'.format(pysize_noborder))
-
+def drawmap_fit(wad, map_name, filename, format, map_type, px_size, py_size, cscheme):
     # --- Load map in editor ---
     edit = MapEditor(wad.maps[map_name])
 
-    # --- Determine scale = map area unit / pixel ---
+    # --- Determine scale = pixel / map unit ---
     xmin = min([v.x for v in edit.vertexes])
     xmax = max([v.x for v in edit.vertexes])
     ymin = min([v.y for v in edit.vertexes])
     ymax = max([v.y for v in edit.vertexes])
-    mapBBox = BBox(xmin, xmax, ymin, ymax)
-    xscale = pxsize_noborder / float(mapBBox.xsize)
-    yscale = pysize_noborder / float(mapBBox.ysize)
-    if VERBOSE:
-        print('drawmap_fit() xmin   = {0}'.format(mapBBox.left))
-        print('drawmap_fit() xmax   = {0}'.format(mapBBox.right))
-        print('drawmap_fit() ymin   = {0}'.format(mapBBox.bottom))
-        print('drawmap_fit() ymax   = {0}'.format(mapBBox.top))
-        print('drawmap_fit() xsize  = {0}'.format(mapBBox.xsize))
-        print('drawmap_fit() ysize  = {0}'.format(mapBBox.ysize))
-        print('drawmap_fit() xscale = {0}'.format(xscale))
-        print('drawmap_fit() yscale = {0}'.format(yscale))
-
-    if xscale < yscale:
-        scale = xscale
-        xoffset = 0
-        yoffset = (pysize - int(mapBBox.ysize*scale)) / 2
-    else:
-        scale = yscale
-        xoffset = (pxsize - int(mapBBox.xsize*scale)) / 2
-        yoffset = 0
-
-    if VERBOSE:
-        print('drawmap_fit() scale   = {0}'.format(scale))
-        print('drawmap_fit() xoffset = {0}'.format(xoffset))
-        print('drawmap_fit() yoffset = {0}'.format(yoffset))
+    LT = LinearTransform(xmin, xmax, ymin, ymax, px_size, py_size, BORDER_PERCENT)
 
     # --- Create image ---
-    im = Image.new('RGB', (pxsize, pysize), cscheme.BG)
+    im = Image.new('RGB', (px_size, py_size), cscheme.BG)
     draw = ImageDraw.Draw(im)
 
+    # --- Draw XY axis ---
+    draw_axis(draw, LT, (200, 200, 200))
+
+    # --- Draw map scale ---
+    draw_scale(draw, LT, (150, 150, 150))
+
+    # --- Draw map ---
     if map_type == MAP_LINEDEFS:
         # --- Draw lines ---
         edit.linedefs.sort(lambda a, b: cmp(not a.two_sided, not b.two_sided))
         for line in edit.linedefs:
-            p1x = scale * (edit.vertexes[line.vx_a].x - mapBBox.left)   + xoffset
-            p1y = scale * (edit.vertexes[line.vx_a].y - mapBBox.bottom) + yoffset
-            p2x = scale * (edit.vertexes[line.vx_b].x - mapBBox.left)   + xoffset
-            p2y = scale * (edit.vertexes[line.vx_b].y - mapBBox.bottom) + yoffset
-            # print('{0} {1} --> {2} {3}'.format(p1x, p1y, p2x, p2y))
+            (p1x, p1y) = LT.MapToScreen(edit.vertexes[line.vx_a].x, edit.vertexes[line.vx_a].y)
+            (p2x, p2y) = LT.MapToScreen(edit.vertexes[line.vx_b].x, edit.vertexes[line.vx_b].y)
 
             color = cscheme.WALL
             if   line.two_sided: color = cscheme.TSWALL
@@ -176,19 +228,19 @@ def drawmap_fit(wad, map_name, filename, format, map_type, pxsize, pysize, csche
             draw_thick_line(draw, p1x, p1y, p2x, p2y, color)
 
         # --- Draw things ---
-        # for thing in edit.things:
-        #     p1x = scale *(thing.x - mapBBox.left) + xoffset
-        #     p1y = scale *(thing.y - mapBBox.bottom) + yoffset
-        #     draw_thing(draw, p1x, p1y, thing.angle, cscheme.THING)
+        for thing in edit.things:
+            # In the future use this function
+            # draw_thing(draw, p1x, p1y, thing.angle, cscheme.THING)
+            color = (0, 255, 0)
+            (px, py) = LT.MapToScreen(thing.x, thing.y)
+            draw.ellipse((px-RADIUS, py-RADIUS, px+RADIUS, py+RADIUS), outline = color)
 
     elif map_type == MAP_VERTEXES:
         # --- Draw lines ---
         edit.linedefs.sort(lambda a, b: cmp(not a.two_sided, not b.two_sided))
         for line in edit.linedefs:
-            p1x = scale * (edit.vertexes[line.vx_a].x - mapBBox.left)   + xoffset
-            p1y = scale * (edit.vertexes[line.vx_a].y - mapBBox.bottom) + yoffset
-            p2x = scale * (edit.vertexes[line.vx_b].x - mapBBox.left)   + xoffset
-            p2y = scale * (edit.vertexes[line.vx_b].y - mapBBox.bottom) + yoffset
+            (p1x, p1y) = LT.MapToScreen(edit.vertexes[line.vx_a].x, edit.vertexes[line.vx_a].y)
+            (p2x, p2y) = LT.MapToScreen(edit.vertexes[line.vx_b].x, edit.vertexes[line.vx_b].y)
             color = cscheme.WALL
             draw_line(draw, p1x, p1y, p2x, p2y, color)
 
@@ -202,12 +254,12 @@ def drawmap_fit(wad, map_name, filename, format, map_type, pxsize, pysize, csche
             # print(vars(vertex))
             color = (0, 255, 255)
             if v_index not in linedefs_vertexes: color = (255, 255, 0)
-            p1x = scale *(edit.vertexes[v_index].x - mapBBox.left) + xoffset
-            p1y = scale *(edit.vertexes[v_index].y - mapBBox.bottom) + yoffset
-            draw.point((p1x, p1y), fill = color)
+            (p1x, p1y) = LT.MapToScreen(edit.vertexes[v_index].x, edit.vertexes[v_index].y)
             draw.rectangle((p1x-SIZE, p1y-SIZE, p1x+SIZE, p1y+SIZE), fill = color)
 
     elif map_type == MAP_SECTORS:
+        # In the future draw the floor textures of each sector, like the map in PrBoom+ when using
+        # OpenGL renderer.
         # --- Draw linedefs ---
         for line in edit.linedefs:
             p1x = scale * (edit.vertexes[line.vx_a].x - mapBBox.left)   + xoffset
@@ -219,6 +271,8 @@ def drawmap_fit(wad, map_name, filename, format, map_type, pxsize, pysize, csche
 
     elif map_type == MAP_NODES:
         # --- Draw segs and ssectors ---
+        # Segs are only defined on linedefs and have implicit edges. I don't know how to
+        # plot the implicit edges.
         # http://doom.wikia.com/wiki/Subsector
         # http://doom.wikia.com/wiki/User_talk:Fraggle#Making_polygons
         # https://www.doomworld.com/vb/doom-editing/43976-subsector-viewer/
@@ -273,138 +327,6 @@ def drawmap_fit(wad, map_name, filename, format, map_type, pxsize, pysize, csche
             draw.point((p1x, p1y), fill = color)
             draw.rectangle((p1x-SIZE, p1y-SIZE, p1x+SIZE, p1y+SIZE), fill = color)
 
-    # --- Draw XY axis ---
-    # NOTE ymin, ymax already inverted!!! This is a workaround
-    # pxmin = (xmin - xmin) * scale + BORDER_PIXELS + xoffset
-    # pxmax = (xmax - xmin) * scale + BORDER_PIXELS + xoffset
-    # pymin = (ymin - ymin) * scale + BORDER_PIXELS + yoffset
-    # pymax = (ymax - ymin) * scale + BORDER_PIXELS + yoffset
-    # pxzero = (0 - xmin) * scale + BORDER_PIXELS + xoffset
-    # pyzero = (0 - ymin) * scale + BORDER_PIXELS + yoffset
-    # color = (255, 0, 0)
-    # draw.line((pxmin, pyzero, pxmax, pyzero), fill = color)
-    # draw.line((pxzero, pymin, pxzero, pymax), fill = color)
-
-    # --- Save image file ---
-    del draw
-    im.save(filename, format)
-
-def drawmap_width(wad, name, filename, format, pxwidth):
-    # Load map in editor
-    edit = MapEditor(wad.maps[name])
-    
-    # Determine scale = map area unit / pixel
-    xmin = min([v.x for v in edit.vertexes])
-    xmax = max([v.x for v in edit.vertexes])
-    ymin = min([-v.y for v in edit.vertexes])
-    ymax = max([-v.y for v in edit.vertexes])
-    xsize = xmax - xmin
-    ysize = ymax - ymin
-    scale = (pxwidth-BORDER_PIXELS*2) / float(max(xsize, ysize))
-    if VERBOSE:
-        print('drawmap_width() xmin  = {0}'.format(xmin))
-        print('drawmap_width() xmax  = {0}'.format(xmax))
-        print('drawmap_width() ymin  = {0}'.format(ymin))
-        print('drawmap_width() ymax  = {0}'.format(ymax))
-        print('drawmap_width() xsize = {0}'.format(xsize))
-        print('drawmap_width() ysize = {0}'.format(ysize))
-        print('drawmap_width() scale = {0}'.format(scale))
-
-    # Convert all numbers to image space
-    pxsize = int(xsize*scale) + BORDER_PIXELS*2;
-    pysize = int(ysize*scale) + BORDER_PIXELS*2;
-    if VERBOSE:
-        print('drawmap_width() pxsize = {0}'.format(pxsize))
-        print('drawmap_width() pysize = {0}'.format(pysize))
-
-    # --- Create image ---
-    im = Image.new('RGB', (pxsize, pysize), (255, 255, 255))
-    draw = ImageDraw.Draw(im)
-
-    # --- Draw lines ---
-    edit.linedefs.sort(lambda a, b: cmp(not a.two_sided, not b.two_sided))
-    for line in edit.linedefs:
-        # >> Flip coordinates of Y axis
-        p1x =  ( edit.vertexes[line.vx_a].x - xmin) * scale + BORDER_PIXELS
-        p1y =  (-edit.vertexes[line.vx_a].y - ymin) * scale + BORDER_PIXELS
-        p2x =  ( edit.vertexes[line.vx_b].x - xmin) * scale + BORDER_PIXELS
-        p2y =  (-edit.vertexes[line.vx_b].y - ymin) * scale + BORDER_PIXELS
-        color = (0, 0, 0)
-        if   line.two_sided: color = (144, 144, 144)
-        elif line.action:    color = (220, 130, 50)
-
-        # >> Draw several lines to simulate thickness 
-        draw.line((p1x, p1y, p2x, p2y), fill = color)
-        draw.line((p1x+1, p1y, p2x+1, p2y), fill = color)
-        draw.line((p1x-1, p1y, p2x-1, p2y), fill = color)
-        draw.line((p1x, p1y+1, p2x, p2y+1), fill = color)
-        draw.line((p1x, p1y-1, p2x, p2y-1), fill = color)
-
-    # --- Draw things ---
-    RADIUS = 5
-    for thing in edit.things:
-        # pprint.pprint(vars(thing))
-
-        # >> Flip coordinates of Y axis
-        p1x =  ( thing.x - xmin) * scale + BORDER_PIXELS
-        p1y =  (-thing.y - ymin) * scale + BORDER_PIXELS
-
-        # >> Change colour depending on flags
-        color = (0, 255, 0)
-        # if   thing.flags <= 2: color = (200,  0,   0)
-        # elif thing.flags <= 4: color = (0,  100, 100)
-        # elif thing.flags <= 8: color = (0,    0, 200)
-
-        # >> Draw circle centered on thing
-        draw.ellipse((p1x-RADIUS, p1y-RADIUS, p1x+RADIUS, p1y+RADIUS), outline = color)
-
-    # --- Draw XY axis ---
-    # NOTE ymin, ymax already inverted!!! This is a workaround
-    pxmin = (xmin - xmin) * scale + BORDER_PIXELS
-    pxmax = (xmax - xmin) * scale + BORDER_PIXELS
-    pymin = (ymin - ymin) * scale + BORDER_PIXELS
-    pymax = (ymax - ymin) * scale + BORDER_PIXELS
-    pxzero = (0 - xmin) * scale + BORDER_PIXELS
-    pyzero = (0 - ymin) * scale + BORDER_PIXELS
-    color = (255, 0, 0)
-    draw.line((pxmin, pyzero, pxmax, pyzero), fill = color)
-    draw.line((pxzero, pymin, pxzero, pymax), fill = color)
-
-    # --- Draw MAP bounding box ---
-    # color = (0, 0, 255)
-    # draw.line((pxmin, pymin, pxmin, pymax), fill = color) # left
-    # draw.line((pxmin, pymin, pxmax, pymin), fill = color) # bottom
-    # draw.line((pxmax, pymin, pxmax, pymax), fill = color) # right
-    # draw.line((pxmin, pymax, pxmax, pymax), fill = color) # top
-
-    # --- Draw grid ---
-    # scale_size = 654 * scale
-    # draw.line((10, 10, 10 + scale_size, 10), fill=(255, 0, 0))
-
-    # --- Draw scale on top-left corner ---
-    # A level must be contained within a 16384-unit radius as measured from its center point. 
-    # A---------B---------C   big gap 327 map units
-    # |         |         |   small gap 163 map units
-    # |         E         |   
-    # D                   F
-    #
-    A_px = (xmin - xmin) * scale + BORDER_PIXELS
-    A_py = (ymax - ymin) * scale + BORDER_PIXELS
-    B_px = (xmin + 163 - xmin) * scale + BORDER_PIXELS
-    B_py = (ymax - ymin) * scale + BORDER_PIXELS
-    C_px = (xmin + 327 - xmin) * scale + BORDER_PIXELS
-    C_py = (ymax - ymin) * scale + BORDER_PIXELS
-    D_px = (xmin - xmin) * scale + BORDER_PIXELS
-    D_py = (ymax - 48 - ymin) * scale + BORDER_PIXELS
-    F_px = (xmin + 327 - xmin) * scale + BORDER_PIXELS
-    F_py = (ymax - 48 - ymin) * scale + BORDER_PIXELS
-
-    color = (123, 104, 238)
-    draw.line((A_px, A_py, B_px, B_py), fill = color) # A -> B
-    draw.line((B_px, B_py, C_px, C_py), fill = color) # B -> C
-    draw.line((A_px, A_py, D_px, D_py), fill = color) # A -> D
-    draw.line((C_px, C_py, F_px, F_py), fill = color) # C -> F
-
     # --- Save image file ---
     del draw
     im.save(filename, format)
@@ -451,14 +373,13 @@ for name in inwad.maps.find(pattern):
     fn_prefix = os.path.splitext(wad_filename)[0] + '_' + name
     fn_sufix  = '.' + format.lower()
     print('Drawing map {0} on file "{1}"'.format(name, fn_prefix))
-    # drawmap_width(inwad, name, filename, format, width)
     # drawmap_fit(inwad, name, fn_prefix + '_Line_A' + fn_sufix, format, MAP_LINEDEFS, 1920, 1080, CDoomWorld)
-    # drawmap_fit(inwad, name, fn_prefix + '_Line_B' + fn_sufix, format, MAP_LINEDEFS, 1920, 1080, CClassic)
+    drawmap_fit(inwad, name, fn_prefix + '_Line_B' + fn_sufix, format, MAP_LINEDEFS, 1920, 1080, CClassic)
     # drawmap_fit(inwad, name, fn_prefix + '_Ver_A' + fn_sufix, format, MAP_VERTEXES, 1920, 1080, CDoomWorld)
-    # drawmap_fit(inwad, name, fn_prefix + '_Ver_B' + fn_sufix, format, MAP_VERTEXES, 1920, 1080, CClassic)
+    drawmap_fit(inwad, name, fn_prefix + '_Ver_B' + fn_sufix, format, MAP_VERTEXES, 1920, 1080, CClassic)
     # drawmap_fit(inwad, name, fn_prefix + '_Sec_A' + fn_sufix, format, MAP_SECTORS, 1920, 1080, CDoomWorld)
     # drawmap_fit(inwad, name, fn_prefix + '_Sec_B' + fn_sufix, format, MAP_SECTORS, 1920, 1080, CClassic)
-    drawmap_fit(inwad, name, fn_prefix + '_Nodes_A' + fn_sufix, format, MAP_NODES, 1920, 1080, CDoomWorld)
-    drawmap_fit(inwad, name, fn_prefix + '_Nodes_B' + fn_sufix, format, MAP_NODES, 1920, 1080, CClassic)
+    # drawmap_fit(inwad, name, fn_prefix + '_Nodes_A' + fn_sufix, format, MAP_NODES, 1920, 1080, CDoomWorld)
+    # drawmap_fit(inwad, name, fn_prefix + '_Nodes_B' + fn_sufix, format, MAP_NODES, 1920, 1080, CClassic)
 
 sys.exit(0)
